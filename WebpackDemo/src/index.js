333c2f3325a084c36b8243ed2d6c7fd61f5f7ce8
SakuraEmotionBot <emotionbot1234@gmail.com>
2018-09-17 16:21:00 +0700

    fix webpack and UI

--- WebpackDemo/src/index.js
+++ WebpackDemo/src/index.js
@@ -1,6 +1,4 @@
-import '../src/style/bootstrap.min.css';
-import '../src/style/font-awesome.min.css';
-import '../src/app.scss';
-import '../src/js/jquery-3.3.1.slim.min.js';
-// import '../src/js/popper.min.js';
-import '../src/js/bootstrap.min.js';
+// import css
+import './style/scss/app.scss';
+// import js
+import './style/js/app';
--- WebpackDemo/webpack.config.js
+++ WebpackDemo/webpack.config.js
@@ -62,12 +62,6 @@ module.exports = {
                     }
                 }]
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
 
             {
                 test: /\.html$/,
